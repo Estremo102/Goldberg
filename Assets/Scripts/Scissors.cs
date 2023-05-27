8d825c40317cc85dde54e9f4d7051b2ed917fbd9
Estremo102 <dominikmozdzen@gmail.com>
2023-05-27 04:50:33 +0200

    scissors delay time

--- Assets/Scripts/Scissors.cs
+++ Assets/Scripts/Scissors.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Scissors : MonoBehaviour
 {
+    [SerializeField] private float TimeToStart;
+    private Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        Invoke("Cut", TimeToStart);
+    }
+
+    private void Cut() => animator.SetBool("Start", true);
 }
