b7188b8c8ab4bbab8d75e162a346aefa21a69374
Estremo102 <dominikmozdzen@gmail.com>
2023-05-30 00:51:03 +0200

    add explosion

--- Assets/Scripts/ExplosionButton.cs
+++ Assets/Scripts/ExplosionButton.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class ExplosionButton : MonoBehaviour
 {
     [SerializeField]
-    private Animator fan;
+    private Animator Fan;
+    [SerializeField]
+    GameObject TNT, Explosion;
+
     private void OnTriggerEnter(Collider other)
     {
-        try
-        {
-            other.GetComponent<ConstantForce>().enabled = false;
-        }
-        finally
-        {
-            fan.SetBool("ButtonPressed", false);
-        }
+        if (other.tag == "Domino")
+            try
+            {
+                other.GetComponent<ConstantForce>().enabled = false;
+            }
+            finally
+            {
+                Fan.SetBool("ButtonPressed", false);
+                Explosion.SetActive(true);
+                TNT.SetActive(false);
+                Invoke("StopExplosion", 4f);
+            }
     }
+
+    private void StopExplosion() => Explosion.SetActive(false);
 }
