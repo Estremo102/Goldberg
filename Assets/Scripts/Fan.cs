0ce254d52164d5baecca3bfc6f32df053e509c49
Estremo102 <dominikmozdzen@gmail.com>
2023-05-29 23:49:26 +0200

    fan wind add

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Fan : MonoBehaviour
 {
     private Animator animator;
+    [SerializeField] 
+    private ConstantForce domino;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -16,6 +19,9 @@ public class Fan : MonoBehaviour
             ActiveFan();
     }
 
-    private void ActiveFan() =>
+    private void ActiveFan()
+    {
         animator.SetBool("ButtonPressed", true);
+        domino.enabled = true;
+    }
 }
