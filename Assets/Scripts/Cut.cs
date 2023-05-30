6a6997109969bd929c9129dfe67d9f909e59e832
Estremo102 <dominikmozdzen@gmail.com>
2023-05-30 04:35:01 +0200

    new materials

--- Assets/Scripts/Cut.cs
+++ Assets/Scripts/Cut.cs
@@ -6,12 +6,21 @@ public class Cut : MonoBehaviour
 {
     [SerializeField]
     Rigidbody m_Rigidbody;
+    [SerializeField]
+    GameObject m_GameObject;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Spr")
         {
-            m_Rigidbody.isKinematic = false;
+            Invoke("C", 0.4f);
+            Invoke("C2", 2f);
         }
     }
+
+    private void C() =>
+        m_Rigidbody.isKinematic = false;
+
+    private void C2() =>
+        m_GameObject.SetActive(false);
 }
